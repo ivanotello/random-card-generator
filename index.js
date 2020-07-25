6a9f5a431943d7f7316126fd06806ad76d739855
ivanotello <iodaruosgg@gmail.com>
2020-07-25 18:45:01 +0000

    HTML, CSS y JS terminados, pero con problemas de comunicacion entre ellos

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 window.onload = () => {
+    function randomNumber(){
+        let number = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+        let randomIndexN = Math.floor(Math.random()*number.length);
+        return number[randomIndexN];
+    };
+    function randomSymbol(){
+        let symbol = ["hearts", "diamonds", "spades", "clubs"];
+        let randomIndexS = Math.floor(Math.random()*symbol.length);
+        return symbol[randomIndexS];
+    };
+
     
-}
+    document.querySelector('.number').innerHTML = randomNumber();
+    document.querySelector('.symbol').classList.add(randomSymbol());
+};
